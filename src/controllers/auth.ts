b35bf13d988f5181b3dbc5b8d9c6d8da8e1903c7
Karol <gilkarol304@gmail.com>
2022-01-30 20:44:50 +0100

    added signup logic

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,11 +1,30 @@
 import { NextFunction, Response } from 'express'
+import bcrypt from 'bcryptjs'
 
-import { Req } from '../util/interfaces'
+import User from '../models/user'
+import { Err, Req } from '../util/interfaces'
 
-export const signup = (req: Req, res: Response, next: NextFunction) => {
-    const email: string = req.body.email
-    const nickname: string = req.body.nickname
-    const password: string = req.body.password
+export const signup = async (req: Req, res: Response, next: NextFunction) => {
+	const email: string = req.body.email
+	const nickname: string = req.body.nickname
+	const password: string = req.body.password
 
-    
+	try {
+		const emailExists = await User.findOne({ email: email })
+		if (emailExists) {
+			const error: Err = new Error('')
+			error.status = 409
+			throw error
+		}
+		const hashedPassword = await bcrypt.hash(password, 12)
+		const user = new User({
+			email: email,
+			nickname: nickname,
+			password: hashedPassword,
+		})
+		await user.save()
+		res.status(200).json({ message: 'User created successfully!' })
+	} catch (err) {
+		next(err)
+	}
 }
