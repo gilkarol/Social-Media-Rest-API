2baeb935fad125620d9a71f80538bd253e2cf10b
Karol <gilkarol304@gmail.com>
2022-01-31 13:26:42 +0100

    added postMessages and getMessages logic

--- src/controllers/messages.ts
+++ src/controllers/messages.ts
@@ -1,5 +1,37 @@
 import { NextFunction, Response } from 'express'
 
-import { Req } from '../util/interfaces'
+import Message from '../models/message'
+import User from '../models/user'
+import { Err, Req } from '../util/interfaces'
 
-export const getMessages = (req: Req, res: Response, next: NextFunction) => {}
+export const getMessages = async (req: Req, res: Response, next: NextFunction) => {
+    try {
+        const messages = await Message.find()
+        res.status(200).json({message: 'Messages found successfully!', messages: messages})
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const postMessages = async (req: Req, res: Response, next: NextFunction) => {
+    const userId: number = req.userId
+    const text: string = req.body.text
+
+    try {
+        const message = new Message({
+            message: text,
+            creatorId: userId
+        })
+        await message.save()
+        const user = await User.findById(userId)
+        if (!user) {
+            const error: Err = new Error('User is not authenticated!')
+            error.status = 401
+            throw error
+        }
+        await user.messages.push(message)
+        res.status(201).json({message: 'Message has been posted successfully!'})
+    } catch (err) {
+        next(err)
+    }
+}
