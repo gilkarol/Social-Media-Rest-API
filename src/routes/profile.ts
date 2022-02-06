9ea756766be5521ae89e5dd7df989f02f221e507
Karol <gilkarol304@gmail.com>
2022-02-06 12:49:16 +0100

    added few routes

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import { getProfile, getFriends, postInviteToFriends, postAcceptInviteToFriends, getInvitedFriends, deleteRemoveFromFriends } from '../controllers/profile'
+import { getProfile, getFriends, postInviteToFriends, postAcceptInviteToFriends, deleteRemoveInviteToFriends, deleteDeclineInviteToFriends, getInvitedFriends, deleteRemoveFromFriends } from '../controllers/profile'
 import isAuth from '../middleware/is-auth'
 
 const router = Router()
@@ -17,6 +17,9 @@ router.post('/friends/accept/:profileId', isAuth, postAcceptInviteToFriends)
 
 router.delete('/friends/remove/:profileId', isAuth, deleteRemoveFromFriends)
 
-router.delete('/friends/remove/:profileId', isAuth, deleteRemoveFromFriends)
+router.delete('/friends/declineInvite/:profileId', isAuth, deleteDeclineInviteToFriends)
+
+router.delete('/friends/removeInvite/:profileId', isAuth, deleteRemoveInviteToFriends)
+
 
 export default router
