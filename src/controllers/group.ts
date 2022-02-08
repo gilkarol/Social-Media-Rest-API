fc8ab38142b9c81f9dbd8ca1a70de8557b367cf0
Karol <gilkarol304@gmail.com>
2022-02-08 11:05:18 +0100

    added getGroupChat

--- src/controllers/group.ts
+++ src/controllers/group.ts
@@ -14,13 +14,35 @@ export const getGroupPosts = async (
 	try {
 		const group = await Group.findById(groupId)
 		if (!group) throw new Err(404, 'Group has not been found!')
-		if (group.particpants.indexOf(profileId) == -1)
+		if (group.participants.indexOf(profileId) == -1)
 			throw new Err(409, 'You are not member of the group!')
 		const posts = group.posts
 
 		res
 			.status(200)
 			.json({ message: 'Posts has been found successfully!', posts: posts })
+	} catch (err) {
+		next(err)
+	}
+}
+
+export const getGroupChat = async (
+	req: Req,
+	res: Response,
+	next: NextFunction
+) => {
+	const groupId: string = req.params.groupId!
+	const profileId: string = req.profileId!
+	try {
+		const group = await Group.findById(groupId)
+		if (!group) throw new Err(404, 'Group has not been found!')
+		if (group.participants.indexOf(profileId) == -1)
+			throw new Err(409, 'You are not member of the group!')
+		const chat = group.chat
+
+		res
+			.status(200)
+			.json({ message: 'Chat has been found successfully!', chat: chat })
 	} catch (err) {
 		next(err)
 	}
