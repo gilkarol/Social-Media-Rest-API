5e457f04e132adc0ebc1c1d8c4ea63513f9987a9
Karol <gilkarol304@gmail.com>
2022-02-09 19:56:39 +0100

    added postPost

--- src/controllers/group.ts
+++ src/controllers/group.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Response } from 'express'
 
 import Group from '../models/group'
+import Post from '../models/post'
 import Profile from '../models/profile'
 import { Err } from '../util/classes'
 import { Req } from '../util/interfaces'
@@ -42,7 +43,6 @@ export const getGroupChat = async (
 		if (group.participants.indexOf(profileId) == -1)
 			throw new Err(409, 'You are not member of the group!')
 		const chat = group.chat
-
 		res
 			.status(200)
 			.json({ message: 'Chat has been found successfully!', chat: chat })
@@ -63,55 +63,88 @@ export const postRequestToJoin = async (
 		const group = await Group.findById(groupId)
 		const profile = await Profile.findById(profileId)
 		if (!group) throw new Err(404, 'This group does not exist!')
-		if (group.participants.indexOf(profileId) >= 0) throw new Err(409, 'You are already member of this group!')
+		if (group.participants.indexOf(profileId) >= 0)
+			throw new Err(409, 'You are already member of this group!')
 		group.joinRequests.push(profile)
 		profile.requestsToJoinGroups.push(group)
 		await group.save()
 		await profile.save()
 	} catch (err) {
-		next (err)
+		next(err)
 	}
 }
 
 export const postPost = async (req: Req, res: Response, next: NextFunction) => {
+	const groupId: string = req.params.groupId
+	const profileId: string = req.profileId!
+	const text: string = req.body.text
 
+	try {
+		const group = await Group.findById(groupId)
+		const profile = await Profile.findById(profileId)
+		if (!group) throw new Err(404, 'This group does not exist!')
+		if (group.participants.indexOf(profile) == -1)
+			throw new Err(409, 'You are not the member of group!')
+		const post = new Post({
+			profile: profile,
+			text: text,
+		})
+		group.posts.push(post)
+		await post.save()
+		await group.save()
+	} catch (err) {
+		next(err)
+	}
 }
 
-export const deletePost = async (req: Req, res: Response, next: NextFunction) => {
-
-} 
-
+export const deletePost = async (
+	req: Req,
+	res: Response,
+	next: NextFunction
+) => {}
 
 // --------------------------- ADMIN ---------------------------
 
-const isAdmin = async () => {
+const isAdmin = async () => {}
 
-}
+export const postCreateGroup = async (
+	req: Req,
+	res: Response,
+	next: NextFunction
+) => {}
 
-export const postCreateGroup = async (req: Req, res: Response, next: NextFunction) => {
+export const postGiveAdmin = async (
+	req: Req,
+	res: Response,
+	next: NextFunction
+) => {}
 
-}
+export const getRequestsToJoin = async (
+	req: Req,
+	res: Response,
+	next: NextFunction
+) => {}
 
-export const postGiveAdmin = async (req: Req, res: Response, next: NextFunction) => {
+export const acceptRequestToJoin = async (
+	req: Req,
+	res: Response,
+	next: NextFunction
+) => {}
 
-}
+export const declineRequestToJoin = async (
+	req: Req,
+	res: Response,
+	next: NextFunction
+) => {}
 
-export const getRequestsToJoin = async (req: Req, res: Response, next: NextFunction) => {
+export const deleteRemoveProfileFromGroup = async (
+	req: Req,
+	res: Response,
+	next: NextFunction
+) => {}
 
-}
-
-export const acceptRequestToJoin = async (req: Req, res: Response, next: NextFunction) => {
-
-}
-
-export const declineRequestToJoin = async (req: Req, res: Response, next: NextFunction) => {
-
-}
-
-export const deleteRemoveProfileFromGroup = async (req: Req, res: Response, next: NextFunction) => {
-
-}
-
-export const deletePostAsAdmin = async (req: Req, res: Response, next: NextFunction) => {
-
-}
+export const deletePostAsAdmin = async (
+	req: Req,
+	res: Response,
+	next: NextFunction
+) => {}
--- src/routes/group.ts
+++ src/routes/group.ts
@@ -1,14 +1,20 @@
 import { Router } from 'express'
-import { getGroupChat, getGroupPosts, postRequestToJoin } from '../controllers/group'
+import { getGroupChat, getGroupPosts, postPost, postRequestToJoin } from '../controllers/group'
 
 import isAuth from '../middleware/is-auth'
 
 const router = Router()
 
+// ------------------------- MEMBERS --------------------------------
+
 router.get('/posts/:groupId', isAuth, getGroupPosts)
+
+router.post('/posts/:groupId', isAuth, postPost)
 
 router.get('/chat/:groupId', isAuth, getGroupChat)
 
 router.post('/requestToJoin/:groupId', isAuth, postRequestToJoin)
+
+// ------------------------- ADMIN --------------------------------
 
 export default router
