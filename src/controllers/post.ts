c88e245d0db2942cda0e49aa2dd27672021a3d68
Karol <gilkarol304@gmail.com>
2022-02-13 20:49:48 +0100

    added patchPost

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -5,6 +5,20 @@ import Profile from '../models/profile'
 import { Err } from '../util/classes'
 import { Req } from '../util/interfaces'
 
+export const getProfilePosts = async (req: Req, res: Response, next: NextFunction) => {
+	const profileId = req.params.profileId
+	const loggedProfileId = req.profileId
+	try {
+		const profile = await Profile.findById(profileId)
+		if (!profile) throw new Err(404, 'This profile does not exist!')
+		if (profile.friends.indexOf(profileId) === -1) throw new Err(409, 'This profile is not your friend!')
+		const posts = profile.posts
+		res.status(200).json({ message: 'Profile posts found successfully!', posts: posts })
+	} catch (err) {
+		next(err)
+	}
+}
+
 export const postPost = async (req: Req, res: Response, next: NextFunction) => {
 	const text = req.body.text
 	const profileId = req.profileId
@@ -23,16 +37,21 @@ export const postPost = async (req: Req, res: Response, next: NextFunction) => {
 	}
 }
 
-export const getProfilePosts = async (req: Req, res: Response, next: NextFunction) => {
-	const profileId = req.params.profileId
-	const loggedProfileId = req.profileId
+export const patchPost = async (req: Req, res: Response, next: NextFunction) => {
+	const text: string = req.body.text
+	const postId: string = req.params.postId
+	const profileId: string = req.profileId!
 	try {
 		const profile = await Profile.findById(profileId)
-		if (!profile) throw new Err(404, 'This profile does not exist!')
-		if (profile.friends.indexOf(profileId) === -1) throw new Err(409, 'This profile is not your friend!')
-		const posts = profile.posts
-		res.status(200).json({ message: 'Profile posts found successfully!', posts: posts })
+		const post = await Post.findById(postId)
+		if (!post) throw new Err(404, 'This post does not exist!')
+		if (profile.posts.indexOf(post) === -1) throw new Err(409, 'You are not creator of the post!')
+		post.text = text
+		await post.save()
+		await profile.save()
+		res.status(200).json({ message: 'Post updated successfully!' })
 	} catch (err) {
 		next(err)
 	}
 }
+
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import isAuth from '../middleware/is-auth'
 
-import {  postPost, getProfilePosts } from '../controllers/post'
+import {  postPost, getProfilePosts, patchPost } from '../controllers/post'
 
 const router = Router()
 
 router.post('/', isAuth, postPost)
 
 router.get('/:profileId', isAuth, getProfilePosts)
+
+router.patch('/:profileId', isAuth, patchPost)
 
 export default router
